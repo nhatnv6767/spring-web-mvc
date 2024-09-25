f08b58ed1e84ce07f682cf063acb989bc9569a32
bhnone <>
2024-09-25 17:33:20 +0900

    MVC in spring : after finish service -> go to write in controller

--- src/main/java/com/dev/btnone/firstspringboot/controller/EmployeeController.java
+++ src/main/java/com/dev/btnone/firstspringboot/controller/EmployeeController.java
@@ -1,12 +1,22 @@
 package com.dev.btnone.firstspringboot.controller;
 
+import com.dev.btnone.firstspringboot.entity.Employee;
+import com.dev.btnone.firstspringboot.service.EmployeeService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.List;
+
 @Controller
 public class EmployeeController {
+
+    @Autowired
+    private EmployeeService employeeService;
+
     @RequestMapping("/employees")
     public String employeeList() {
+        List<Employee> employees = employeeService.getListEmployee();
         // ten cua 1 view, dat mac dinh trong folder template
         return "employee-view";
     }
