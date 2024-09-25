19266dfc9f0ceb32a29a9956987265dffb1a325b
bhnone <>
2024-09-25 17:32:03 +0900

    MVC in spring : viet tu repository truoc roi toi service

--- src/main/java/com/dev/btnone/firstspringboot/repository/EmployeeRepository.java
+++ src/main/java/com/dev/btnone/firstspringboot/repository/EmployeeRepository.java
@@ -1,7 +1,14 @@
 package com.dev.btnone.firstspringboot.repository;
 
+import com.dev.btnone.firstspringboot.entity.Employee;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public class EmployeeRepository {
+
+    public List<Employee> getEmployeeFromDB() {
+        return List.of(new Employee(1l, "John", 18), new Employee(2l, "John 2", 19), new Employee(3l, "John 3", 22));
+    }
 }
--- src/main/java/com/dev/btnone/firstspringboot/service/EmployeeService.java
+++ src/main/java/com/dev/btnone/firstspringboot/service/EmployeeService.java
@@ -1,9 +1,19 @@
 package com.dev.btnone.firstspringboot.service;
 
+import com.dev.btnone.firstspringboot.entity.Employee;
+import com.dev.btnone.firstspringboot.repository.EmployeeRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class EmployeeService {
+    @Autowired
+    private EmployeeRepository employeeRepository;
 
+    public List<Employee> getListEmployee() {
+        return employeeRepository.getEmployeeFromDB();
+    }
 
 }
